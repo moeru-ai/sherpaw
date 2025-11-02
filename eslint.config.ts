adc030965dbbc06b0d53527bc648a8d0286c0714
Makito <i@maki.to>
2025-11-03 02:33:47 +0900

    feat: asr package

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,8 @@
 import eslintConfig from '@antfu/eslint-config'
 
-export default eslintConfig()
+export default eslintConfig({
+  ignores: [
+    'packages/asr/src/sherpa-onnx-asr.js',
+    'packages/asr/src/sherpa-onnx-wasm-main-asr.js',
+  ],
+})
---
+++ packages/asr/src/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line ts/ban-ts-comment
+// @ts-nocheck
+
+export * from './sherpa-onnx-asr'
+export * as AsrModule from './sherpa-onnx-wasm-main-asr'
