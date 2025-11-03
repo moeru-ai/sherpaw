7d62351b34a12e36dcfa7dde11916a77b4682494
Makito <i@maki.to>
2025-11-04 04:21:13 +0900

    feat(shared): update types

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -1,3 +1,22 @@
-export interface WasmModuleOptions {
+/// <reference types="emscripten" />
+
+export interface WasmModule extends EmscriptenModule {
+  calledRun?: boolean
+  runtimeInitialized?: boolean
+
+  setValue: typeof setValue
+  getValue: typeof getValue
+
+  stringToUTF8: typeof stringToUTF8
+  UTF8ToString: typeof UTF8ToString
+  lengthBytesUTF8: typeof lengthBytesUTF8
+
+  FS_createDataFile: typeof FS['createDataFile']
+
+  addRunDependency: typeof addRunDependency
+  removeRunDependency: typeof removeRunDependency
+}
+
+export interface WasmModuleOptions extends Partial<WasmModule> {
   locateFile: (path: string) => string
 }
