4f6a58a1e2cedd3524188869361f0835ff04d5d0
Makito <i@maki.to>
2025-11-04 02:04:51 +0900

    chore(eslint): update ignores

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,8 @@ import eslintConfig from '@antfu/eslint-config'
 export default eslintConfig({
   ignores: [
     'packages/asr/src/sherpa-onnx-asr.js',
+    'packages/asr/src/sherpa-onnx-wasm-main-asr.d.ts',
     'packages/asr/src/sherpa-onnx-wasm-main-asr.js',
   ],
+  vue: true,
 })
