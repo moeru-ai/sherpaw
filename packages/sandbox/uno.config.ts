7886dcb439ddc5843a495f5068f450e03c6ff32c
Makito <i@maki.to>
2025-11-06 03:08:18 +0900

    feat: add unocss

---
+++ packages/sandbox/uno.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, presetAttributify, presetWebFonts, presetWind4 } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetWind4(),
+    presetAttributify(),
+    presetWebFonts({
+      fonts: {
+        sans: {
+          name: 'Zalando Sans SemiExpanded',
+          weights: [400, 600, 700, 900],
+        },
+        mono: {
+          name: 'Google Sans Code',
+          weights: [400, 700],
+        },
+      },
+    }),
+  ],
+})
