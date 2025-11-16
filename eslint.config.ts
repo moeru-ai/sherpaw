2662827da73a86fe8c856bb9adab079286049ab4
Makito <i@maki.to>
2025-11-16 18:36:22 +0900

    chore: update eslint ignores

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,8 +3,9 @@ import eslintConfig from '@antfu/eslint-config'
 export default eslintConfig({
   ignores: [
     'packages/asr/src/sherpa-onnx-asr.js',
-    'packages/asr/src/sherpa-onnx-wasm-main-asr.d.ts',
     'packages/asr/src/sherpa-onnx-wasm-main-asr.js',
+    'packages/speaker-diarization/src/sherpa-onnx-speaker-diarization.js',
+    'packages/speaker-diarization/src/sherpa-onnx-wasm-main-speaker-diarization.js',
   ],
   vue: true,
 })
