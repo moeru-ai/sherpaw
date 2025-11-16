bcdc27e5640cfcbfc6e679be4d7d9b0a0b7d674a
Makito <i@maki.to>
2025-11-16 18:35:08 +0900

    fix(asr): better ts-expect-error

--- packages/asr/src/index.ts
+++ packages/asr/src/index.ts
@@ -1,6 +1,4 @@
-// eslint-disable-next-line ts/ban-ts-comment
-// @ts-nocheck
-
 export * from './sherpa-onnx-asr'
+// @ts-expect-error Missing types
 export { default as initASRModule } from './sherpa-onnx-wasm-main-asr'
 export type { WebAssemblyModule as ASRModule } from '@sherpa-onnx-wasm/shared'
