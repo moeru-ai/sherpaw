04b2233c41fa6f0c4711bede19f8c14d0ec5e07e
Neko Ayaka <neko@ayaka.moe>
2025-11-17 23:00:37 +0800

    refactor: use variable

--- models/sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20/pack.sh
+++ models/sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20/pack.sh
@@ -2,12 +2,18 @@
 set -e
 cd $(dirname "$0")/../..
 
+MODULE_SHORT=ASR
+MODULE_SHORT_LOWER=asr
+MODEL_NAME="sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20"
+
 docker run --platform linux/amd64 \
   -v "$PWD/upstream/sherpa-onnx:/opt/sherpa-onnx" \
-  -v "$PWD/models/sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20/model/normalized:/opt/sherpa-onnx/wasm/asr/assets" \
-  -v "$PWD/models/sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20/install/bin/wasm:/opt/sherpa-onnx/build-wasm-simd-asr/install/bin/wasm/asr" \
-  -e SHERPA_ONNX_WASM_ASR_SKIP_PRELOAD=ON \
-  -e SHERPA_ONNX_WASM_ASR_EXPORT_ES6=ON \
+  -v "$PWD/models/$MODEL_NAME/model/normalized:/opt/sherpa-onnx/wasm/${MODULE_SHORT_LOWER}/assets" \
+  -v "$PWD/models/$MODEL_NAME/install/bin/wasm:/opt/sherpa-onnx/build-wasm-simd-${MODULE_SHORT_LOWER}/install/bin/wasm/${MODULE_SHORT_LOWER}" \
+  -e SHERPA_ONNX_WASM_${MODULE_SHORT}_SKIP_PRELOAD=ON \
+  -e SHERPA_ONNX_WASM_${MODULE_SHORT}_EXPORT_ES6=ON \
+  -e MODULE_SHORT=${MODULE_SHORT} \
+  -e MODULE_SHORT_LOWER=${MODULE_SHORT_LOWER} \
   --entrypoint /bin/bash \
   emscripten/emsdk \
   -lc 'cd /opt/sherpa-onnx/wasm/asr && \
@@ -17,6 +23,6 @@ docker run --platform linux/amd64 \
          --js-output=preload.js \
          --separate-metadata \
          --export-es6 && \
-       mv preload.data /opt/sherpa-onnx/build-wasm-simd-asr/install/bin/wasm/asr/preload.data && \
-       mv preload.js /opt/sherpa-onnx/build-wasm-simd-asr/install/bin/wasm/asr/preload.js && \
-       mv preload.js.metadata /opt/sherpa-onnx/build-wasm-simd-asr/install/bin/wasm/asr/preload.js.metadata'
+       mv preload.data /opt/sherpa-onnx/build-wasm-simd-${MODULE_SHORT_LOWER}/install/bin/wasm/${MODULE_SHORT_LOWER}/preload.data && \
+       mv preload.js /opt/sherpa-onnx/build-wasm-simd-${MODULE_SHORT_LOWER}/install/bin/wasm/${MODULE_SHORT_LOWER}/preload.js && \
+       mv preload.js.metadata /opt/sherpa-onnx/build-wasm-simd-${MODULE_SHORT_LOWER}/install/bin/wasm/${MODULE_SHORT_LOWER}/preload.js.metadata'
