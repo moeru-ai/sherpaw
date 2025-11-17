c032225db5bc552518cea43f983fc6626df9651c
Neko <neko@ayaka.moe>
2025-11-17 23:03:34 +0800

    feat(ci): basic build workflow pattern for models (#1)
    * feat(ci): basic build workflow pattern for models
    
    * feat(ci): update script
    
    * fix: remove
    
    * fix: follow build-wasm.sh
    
    * refactor: use variable
    
    * fix: cleanup unused emcc
    
    * fix: cleanup unused emcc

---
+++ models/sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname "$0")/../..
+
+MODULE_SHORT=ASR
+MODULE_SHORT_LOWER=asr
+MODEL_NAME="sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20"
+
+docker run --platform linux/amd64 \
+  -v "$PWD/upstream/sherpa-onnx:/opt/sherpa-onnx" \
+  -v "$PWD/models/$MODEL_NAME/model/normalized:/opt/sherpa-onnx/wasm/${MODULE_SHORT_LOWER}/assets" \
+  -v "$PWD/models/$MODEL_NAME/install/bin/wasm:/opt/sherpa-onnx/build-wasm-simd-${MODULE_SHORT_LOWER}/install/bin/wasm/${MODULE_SHORT_LOWER}" \
+  -e SHERPA_ONNX_WASM_${MODULE_SHORT}_SKIP_PRELOAD=ON \
+  -e SHERPA_ONNX_WASM_${MODULE_SHORT}_EXPORT_ES6=ON \
+  -e MODULE_SHORT=${MODULE_SHORT} \
+  -e MODULE_SHORT_LOWER=${MODULE_SHORT_LOWER} \
+  --entrypoint /bin/bash \
+  emscripten/emsdk \
+  -lc 'emcc -v && \
+       cd /opt/sherpa-onnx && \
+       /opt/sherpa-onnx/build-wasm-simd-${MODULE_SHORT_LOWER}.sh'
