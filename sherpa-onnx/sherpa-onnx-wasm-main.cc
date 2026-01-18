b74913f9772e0fe6a5f7054202f208f109213e54
Makito <i@maki.to>
2026-01-19 01:35:27 +0900

    fix(build): add the missing CopyHeap export

--- sherpa-onnx/sherpa-onnx-wasm-main.cc
+++ sherpa-onnx/sherpa-onnx-wasm-main.cc
@@ -4,3 +4,9 @@
 #include <memory>
 
 #include "sherpa-onnx/c-api/c-api.h"
+
+extern "C" {
+  void CopyHeap(const char *src, int32_t num_bytes, char *dst) {
+    std::copy(src, src + num_bytes, dst);
+  }
+}
